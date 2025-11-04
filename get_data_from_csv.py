b8ecca1ef5d6eaefb4a61aab50f35fbe928cb63d
AlekseiKim <kimalex8589@gmail.com>
2025-11-05 01:18:34 +0300

    Конфигурация

---
+++ get_data_from_csv.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+netflix_data = pd.read_csv('/Users/kimaleksei/Documents/Python projects/Project 4. Netflix data/netflix_titles.csv')
---
+++ main.py
@@ -0,0 +1,3 @@
+from get_data_from_csv import netflix_data
+
+print(netflix_data.info)
